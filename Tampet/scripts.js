cfb03469f1fce676d021a199ede88db3c0fe0cc1
LuizSeabraDeMarco <139667471+LuizSeabraDeMarco@users.noreply.github.com>
2024-01-12 15:23:48 -0300

    Tampet

--- Tampet/scripts.js
+++ Tampet/scripts.js
@@ -1,12 +1,9 @@
-function clickMenu() {
-    var filtro = document.getElementById("menu");
-    var header = document.getElementById("header")
-    if (filtro.style.display == "inline-block" || header.style.height == "31vh"){
-        header.style.height = "7vh";
-        filtro.style.display = "none";
 
-    }else{
-        header.style.height = "31vh";
-        filtro.style.display = "inline-block"
-    }
-    }
+function link_insta(){
+    window.location.href = "https://www.instagram.com/tampet_sulfluminense/";
+}
+
+function voltar() {
+    history.back();
+  }
+  
